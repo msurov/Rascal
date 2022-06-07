42ee9f2e8d2b9b8b7fdd9f739bfd1fa464366f47
Maksim Surov <surov@arrival.com>
2022-06-07 18:19:00 +0300

    fuel tank removed

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ fgfs --aircraft=Rascal110-JSBSim --fg-aircraft="./" \
     --airport=KRNO --runway=34L --timeofday=noon \
     --disable-real-weather-fetch \
     --wind=0@0 \
-    --generic=file,out,10,/tmp/data.bin,jsbsimlogbin
+    --generic=file,out,10,/tmp/data.bin,jsblogbin
